8dc36ec3329ca76c3d090ca16203ef5562e33faa
Rohit <rohit.k1999@gmail.com>
2021-10-22 10:53:44 +0530

    my project

--- root/settings.py
+++ root/settings.py
@@ -121,3 +121,5 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
